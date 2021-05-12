cf8d2eb6fc3919aa3cbb157197f2a5fd71a2d365
Tyler Garman <tylergarman276@gmail.com>
2021-05-11 20:31:29 -0500

    Moved the navbar to its own component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  
+  @Input() activeElement: string;
+  
+  constructor() {
+    this.activeElement = "none";
+  }
+
+  ngOnInit(): void {
+  }
+
+}
